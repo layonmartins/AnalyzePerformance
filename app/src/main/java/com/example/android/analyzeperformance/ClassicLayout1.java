33302095e387181a78753c545db5181d2d5d1f7a
Layon Martins <layonmartins03@gmail.com>
2018-11-07 08:50:52 -0200

    first commit

---
+++ app/src/main/java/com/example/android/analyzeperformance/ClassicLayout1.java
@@ -0,0 +1,13 @@
+package com.example.android.analyzeperformance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassicLayout1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classic_layout1);
+    }
+}
---
+++ app/src/main/java/com/example/android/analyzeperformance/activity_classic_layout3.java
@@ -0,0 +1,13 @@
+package com.example.android.analyzeperformance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_classic_layout3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classic_layout3);
+    }
+}
