04924029964e8f9c42ebdc1a8f32acb234fcc69d
Layon Martins <layonmartins03@gmail.com>
2018-12-13 09:06:55 -0200

    add the spinner layouts

--- app/src/main/java/com/example/android/analyzeperformance/LinearLayout.java
+++ app/src/main/java/com/example/android/analyzeperformance/LinearLayout.java
@@ -2,15 +2,23 @@ package com.example.android.analyzeperformance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class LinearLayout extends AppCompatActivity {
+public class LinearLayout extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    //Int layout
+    //Linear = 0, Relative = 1, Constraint = 2
+    int layout = 0;
 
     //Array info examples
     Integer[] imageArray = {R.drawable.boy, R.drawable.women, R.drawable.girl};
@@ -61,6 +69,17 @@ public class LinearLayout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
 
+        //Config the spinner layout:
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+                (this, R.array.layouts_array, R.layout.my_spinner);
+        //Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.my_spinner);
+        //Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        
         final Button button = findViewById(R.id.btnScroll);
         button.setOnClickListener(new View.OnClickListener(){
 
@@ -70,6 +89,17 @@ public class LinearLayout extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+        //An item was selected. You can retrieve the selected item using parent.getItemAtPosition(pos)
+        layout = pos;
+
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent){
+        //Another interface callback
     }
 
 }
