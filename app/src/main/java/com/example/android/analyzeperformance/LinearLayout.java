5e9ea0edba7153b757c8ff127b825e1d3ada0a7c
Layon Martins <layonmartins03@gmail.com>
2018-12-17 10:08:20 -0200

    implement scrolldown automatically and generate list when user input the employees number

--- app/src/main/java/com/example/android/analyzeperformance/LinearLayout.java
+++ app/src/main/java/com/example/android/analyzeperformance/LinearLayout.java
@@ -3,13 +3,16 @@ package com.example.android.analyzeperformance;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +21,11 @@ public class LinearLayout extends AppCompatActivity implements AdapterView.OnIte
 
     //Int layout
     //Linear = 0, Relative = 1, Constraint = 2
-    int layout = 0;
+    int layout_ID = 0;
+    //number of employees in the list
+    int n = 0;
+    //EditText to the number of employees
+    EditText editText;
 
     //Array info examples
     Integer[] imageArray = {R.drawable.boy, R.drawable.women, R.drawable.girl};
@@ -46,16 +53,15 @@ public class LinearLayout extends AppCompatActivity implements AdapterView.OnIte
         return employees;
     }
 
-    //Generate the list employees example, setAdapter and call the scrolldown method
+    //Generate the list employees example, setAdapter
     private void generateList(int layout){
 
-        EditText editText = (EditText) findViewById(R.id.edtNumber);
-        int n = Integer.parseInt(editText.getText().toString());
+        editText = (EditText) findViewById(R.id.edtNumber);
+        n = Integer.parseInt(editText.getText().toString());
         List<Employee> employees = getEmployees(n);
         CustomListAdapter myAdapter = new CustomListAdapter(this, employees, getLayout(layout));
         listView = (ListView) findViewById(R.id.listViewID);
         listView.setAdapter(myAdapter);
-        //call the scrolldown method
     }
 
     private int getLayout(int layout){
@@ -70,12 +76,6 @@ public class LinearLayout extends AppCompatActivity implements AdapterView.OnIte
         }
         return returnLayout;
     }
-
-    //scrolldown the list until final
-    private void scrolldown(){
-
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,13 +92,34 @@ public class LinearLayout extends AppCompatActivity implements AdapterView.OnIte
         //Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+        //first called to generate a List
+        generateList(layout_ID);
 
+        //generate list when user input the number of employees
+        editText.setOnEditorActionListener(new EditText.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                // Identifier of the action. This will be either the identifier you supplied,
+                // or EditorInfo.IME_NULL if being called due to the enter key being pressed.
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    //generate list always user click in "done" in EditText number
+                    generateList(layout_ID);
+                    //return true; //if return true the event dies here and the keyboard does not close automatically
+                }
+                return false;
+            }
+        });
+
+        //scroll down automatically
         final Button button = findViewById(R.id.btnScroll);
         button.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v){
 
-                generateList(layout);
+                listView.smoothScrollToPosition(n);
             }
         });
 
@@ -107,7 +128,7 @@ public class LinearLayout extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
         //An item was selected. You can retrieve the selected item using parent.getItemAtPosition(pos)
-        layout = pos;
+        layout_ID = pos;
 
     }
     @Override
